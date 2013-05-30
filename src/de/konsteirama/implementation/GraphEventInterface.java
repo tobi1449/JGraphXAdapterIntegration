5a244ad0584573440404bc800221b88cd9c23268
Tobias <t-zimmermann@nlinehome.de>
2013-05-30 18:03:54 +0200

    I hate git (really added the sub interfaces this time) added basic implementation of DrawingLibraryInterface so the jgrapht-group can start implementing

---
+++ src/de/konsteirama/implementation/GraphEventInterface.java
@@ -0,0 +1,15 @@
+package de.konsteirama.implementation;
+
+import java.awt.event.MouseAdapter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tobias
+ * Date: 30.05.13
+ * Time: 17:51
+ */
+public interface GraphEventInterface {
+
+    public void registerMouseAdapter(MouseAdapter adapter);
+
+}
