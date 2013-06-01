ce10f714305963c2b004f5fe06412502aeacc73f
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2013-06-01 15:57:42 +0200

    Hotfix for JGraphXAdapterTest
    Fixed .equals not working due to different classes

--- src/de/konsteirama/tests/JGraphXAdapterTests.java
+++ src/de/konsteirama/tests/JGraphXAdapterTests.java
@@ -2,14 +2,15 @@ package de.konsteirama.tests;
 
 import static org.junit.Assert.fail;
 
+import java.util.Collection;
 import java.util.HashMap;
-
+import java.util.HashSet;
+import java.util.Set;
 
 import org.jgrapht.ListenableGraph;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.ListenableDirectedGraph;
 import org.junit.Test;
-
 
 import com.mxgraph.model.mxCell;
 
@@ -19,180 +20,220 @@ import de.konsteirama.jgraphxadapter.JGraphXAdapter;
  * Test methods for the class JGraphXAdapter.
  * 
  * @author KonSteiRaMa
- *
+ * 
  */
 public class JGraphXAdapterTests {
 
-	/** 
-	 * Test scenarios under normal conditions. 
-	 */
-	@Test
-	public final void genericTest() {
-		ListenableGraph<String, DefaultEdge> jGraphT = 
-			new ListenableDirectedGraph<String, DefaultEdge>(DefaultEdge.class);
+    /**
+     * Test scenarios under normal conditions.
+     */
+    @Test
+    public final void genericTest() {
+        ListenableGraph<String, DefaultEdge> jGraphT 
+         = new ListenableDirectedGraph<String, DefaultEdge>(DefaultEdge.class);
 
-		// fill graph with data
-		String v1 = "Vertex 1";
-		String v2 = "Vertex 2";
-		String v3 = "Vertex 3";
-		String v4 = "Vertex 4";
+        // fill graph with data
+        String v1 = "Vertex 1";
+        String v2 = "Vertex 2";
+        String v3 = "Vertex 3";
+        String v4 = "Vertex 4";
 
-		jGraphT.addVertex(v1);
-		jGraphT.addVertex(v2);
-		jGraphT.addVertex(v3);
-		jGraphT.addVertex(v4);
+        jGraphT.addVertex(v1);
+        jGraphT.addVertex(v2);
+        jGraphT.addVertex(v3);
+        jGraphT.addVertex(v4);
 
-		jGraphT.addEdge(v1, v2);
-		jGraphT.addEdge(v1, v3);
-		jGraphT.addEdge(v1, v4);
-		jGraphT.addEdge(v2, v3);
-		jGraphT.addEdge(v3, v4);
+        jGraphT.addEdge(v1, v2);
+        jGraphT.addEdge(v1, v3);
+        jGraphT.addEdge(v1, v4);
+        jGraphT.addEdge(v2, v3);
+        jGraphT.addEdge(v3, v4);
 
-		// Create jgraphx graph and test it
-		JGraphXAdapter<String, DefaultEdge> graphX = 
-				new JGraphXAdapter<String, DefaultEdge>(jGraphT);
-		testMapping(graphX);
+        // Create jgraphx graph and test it
+        JGraphXAdapter<String, DefaultEdge> graphX = 
+                new JGraphXAdapter<String, DefaultEdge>(jGraphT);
+        testMapping(graphX);
 
-		try {
-			graphX.addJGraphTEdge(new DefaultEdge());
-			graphX.addJGraphTEdge(new DefaultEdge());
-			graphX.addJGraphTEdge(new DefaultEdge());
+        try {
+            graphX.addJGraphTEdge(new DefaultEdge());
+            graphX.addJGraphTEdge(new DefaultEdge());
+            graphX.addJGraphTEdge(new DefaultEdge());
 
-			graphX.addJGraphTVertex("New Vertex 1");
-			graphX.addJGraphTVertex("New Vertex 2");
-			graphX.addJGraphTVertex("New Vertex 3");
+            graphX.addJGraphTVertex("New Vertex 1");
+            graphX.addJGraphTVertex("New Vertex 2");
+            graphX.addJGraphTVertex("New Vertex 3");
 
-			// Add the same vertex 2 times
-			String v = "Some Vertex";
-			graphX.addJGraphTVertex(v);
-			graphX.addJGraphTVertex(v);
+            // Add the same vertex 2 times
+            String v = "Some Vertex";
+            graphX.addJGraphTVertex(v);
+            graphX.addJGraphTVertex(v);
 
-			// Add the same edge 2 times
-			DefaultEdge edge = new DefaultEdge();
-			graphX.addJGraphTEdge(edge);
-			graphX.addJGraphTEdge(edge);
+            // Add the same edge 2 times
+            DefaultEdge edge = new DefaultEdge();
+            graphX.addJGraphTEdge(edge);
+            graphX.addJGraphTEdge(edge);
 
-		} catch (Exception e) {
-			fail("Unexpected error during adding new edges or vertices");
-		}
+        } catch (Exception e) {
+            fail("Unexpected error during adding new edges or vertices");
+        }
 
-		testMapping(graphX);
+        testMapping(graphX);
 
-		// add some data to the jgrapht graph - changes should be propagated
-		// through jgraphxadapters graphlistener interface
+        // add some data to the jgrapht graph - changes should be propagated
+        // through jgraphxadapters graphlistener interface
 
-		String v5 = "Vertex 5";
-		String v6 = "Vertex 6";
-		String v7 = "Vertex 7";
-		String v8 = "Vertex 8";
+        String v5 = "Vertex 5";
+        String v6 = "Vertex 6";
+        String v7 = "Vertex 7";
+        String v8 = "Vertex 8";
 
-		jGraphT.addVertex(v5);
-		jGraphT.addVertex(v6);
-		jGraphT.addVertex(v7);
-		jGraphT.addVertex(v8);
+        jGraphT.addVertex(v5);
+        jGraphT.addVertex(v6);
+        jGraphT.addVertex(v7);
+        jGraphT.addVertex(v8);
 
-		jGraphT.addEdge(v5, v6);
-		jGraphT.addEdge(v5, v7);
-		jGraphT.addEdge(v5, v8);
-		jGraphT.addEdge(v6, v7);
-		jGraphT.addEdge(v7, v8);
+        jGraphT.addEdge(v5, v6);
+        jGraphT.addEdge(v5, v7);
+        jGraphT.addEdge(v5, v8);
+        jGraphT.addEdge(v6, v7);
+        jGraphT.addEdge(v7, v8);
 
-		testMapping(graphX);
-	}
+        testMapping(graphX);
+    }
 
-	/**
-	 * Tests conditions if graph is initialized without a JgraphT graph.
-	 */
-	@Test
-	public final void nullInitializationTest() {
-		JGraphXAdapter<String, String> graph = null;
+    /**
+     * Tests conditions if graph is initialized without a JgraphT graph.
+     */
+    @Test
+    public final void nullInitializationTest() {
+        JGraphXAdapter<String, String> graph = null;
 
-		try {
-			graph = new JGraphXAdapter<String, String>(null);
-		} catch (Exception e) {
-			fail("Unexpected error encountered during " 
-					+ " creation of JGraphXAdapter with null");
-		}
+        try {
+            graph = new JGraphXAdapter<String, String>(null);
+        } catch (Exception e) {
+            fail("Unexpected error encountered during "
+                    + " creation of JGraphXAdapter with null");
+        }
 
-		testMapping(graph);
+        testMapping(graph);
 
-		try {
-			graph.addJGraphTEdge("New Edge 1");
-			graph.addJGraphTEdge("New Edge 2");
-			graph.addJGraphTEdge("New Edge 3");
+        try {
+            graph.addJGraphTEdge("New Edge 1");
+            graph.addJGraphTEdge("New Edge 2");
+            graph.addJGraphTEdge("New Edge 3");
 
-			graph.addJGraphTVertex("New Vertex 1");
-			graph.addJGraphTVertex("New Vertex 2");
-			graph.addJGraphTVertex("New Vertex 3");
-		} catch (Exception e) {
-			fail("Unexpected error during adding new edges or vertices");
-		}
+            graph.addJGraphTVertex("New Vertex 1");
+            graph.addJGraphTVertex("New Vertex 2");
+            graph.addJGraphTVertex("New Vertex 3");
+        } catch (Exception e) {
+            fail("Unexpected error during adding new edges or vertices");
+        }
 
-		testMapping(graph);
-	}
+        testMapping(graph);
+    }
 
-	/**
-	 * Tests the mapping of the graph for consistency. Mapping includes: -
-	 * getCellToEdgeMap - getEdgeToCellMap - getCellToVertexMap -
-	 * getVertexToCellMap
-	 * 
-	 * @param graph
-	 * The graph to be tested
-	 * 
-	 * @param <E>
-	 * The class used for the edges of the JGraphXAdapter
-	 * 
-	 * @param <V>
-	 * The class used for the vertices of the JGraphXAdapter
-	 */
-	private <V, E> void testMapping(final JGraphXAdapter<V, E> graph) {
+    /**
+     * Tests the mapping of the graph for consistency. Mapping includes: -
+     * getCellToEdgeMap - getEdgeToCellMap - getCellToVertexMap -
+     * getVertexToCellMap
+     * 
+     * @param graph
+     *            The graph to be tested
+     * 
+     * @param <E>
+     *            The class used for the edges of the JGraphXAdapter
+     * 
+     * @param <V>
+     *            The class used for the vertices of the JGraphXAdapter
+     */
+    private <V, E> void testMapping(final JGraphXAdapter<V, E> graph) {
 
-		// Edges
-		HashMap<mxCell, E> cellToEdgeMap = graph.getCellToEdgeMap();
-		HashMap<E, mxCell> edgeToCellMap = graph.getEdgeToCellMap();
+        // Edges
+        HashMap<mxCell, E> cellToEdgeMap = graph.getCellToEdgeMap();
+        HashMap<E, mxCell> edgeToCellMap = graph.getEdgeToCellMap();
 
-		// Test for null
-		if (cellToEdgeMap == null) {
-			fail("GetCellToEdgeMap returned null");
-		}
+        // Test for null
+        if (cellToEdgeMap == null) {
+            fail("GetCellToEdgeMap returned null");
+        }
 
-		if (edgeToCellMap == null) {
-			fail("GetEdgeToCellMap returned null");
-		}
+        if (edgeToCellMap == null) {
+            fail("GetEdgeToCellMap returned null");
+        }
 
-		// Compare keys to values
-		if (!cellToEdgeMap.keySet().equals(edgeToCellMap.values())) {
-			fail("CellToEdgeMap has not the "
-					+ "same keys as the values in EdgeToCellMap");
-		}
+        // Compare keys to values
+        if (!compare(edgeToCellMap.values(), cellToEdgeMap.keySet())) {
+            fail("CellToEdgeMap has not the "
+                    + "same keys as the values in EdgeToCellMap");
+        }
 
-		if (!edgeToCellMap.keySet().equals(cellToEdgeMap.values())) {
-			fail("EdgeToCellMap has not the "
-					+ "same keys as the values in CellToEdgeMap");
-		}
+        if (!compare(cellToEdgeMap.values(), edgeToCellMap.keySet())) {
+            fail("EdgeToCellMap has not the "
+                    + "same keys as the values in CellToEdgeMap");
+        }
 
-		// Vertices
-		HashMap<mxCell, V> cellToVertexMap = graph.getCellToVertexMap();
-		HashMap<V, mxCell> vertexToCellMap = graph.getVertexToCellMap();
+        // Vertices
+        HashMap<mxCell, V> cellToVertexMap = graph.getCellToVertexMap();
+        HashMap<V, mxCell> vertexToCellMap = graph.getVertexToCellMap();
 
-		// Test for null
-		if (cellToVertexMap == null) {
-			fail("GetVertexToCellMap returned null");
-		}
+        // Test for null
+        if (cellToVertexMap == null) {
+            fail("GetVertexToCellMap returned null");
+        }
 
-		if (vertexToCellMap == null) {
-			fail("GetCellToVertexMap returned null");
-		}
+        if (vertexToCellMap == null) {
+            fail("GetCellToVertexMap returned null");
+        }
 
-		// Compare keys to values
-		if (!cellToVertexMap.keySet().equals(vertexToCellMap.values())) {
-			fail("CellToVertexMap has not the same "
-					+ "keys as the values in VertexToCellMap");
-		}
+        // Compare keys to values
+        if (!compare(vertexToCellMap.values(), cellToVertexMap.keySet())) {
+            fail("CellToVertexMap has not the same "
+                    + "keys as the values in VertexToCellMap");
+        }
 
-		if (!vertexToCellMap.keySet().equals(cellToVertexMap.values())) {
-			fail("VertexToCellMap has not the same "
-					+ "keys as the values in CellToVertexMap");
-		}
-	}
+        if (!compare(cellToVertexMap.values(), vertexToCellMap.keySet())) {
+            fail("VertexToCellMap has not the same "
+                    + "keys as the values in CellToVertexMap");
+        }
+    }
+
+    /**
+     * Compares a collection to a set. Returns true if the specified parameters
+     * have the same size, and every member of the specified parameters is
+     * contained in this set (or equivalently, every member of the parameter is
+     * contained in the specified set).
+     * 
+     * @param collection
+     *            The collection that is compared
+     * 
+     * @param set
+     *            The set that is compared
+     * 
+     * @param <T>
+     *            The classtype of the set and collection.
+     * 
+     * @return True, if set and collection are equivalent; False if not.
+     * 
+     */
+    private <T> boolean compare(final Collection<T> collection, final Set<T> set) {
+
+        Set<T> compareSet = new HashSet<T>();
+        compareSet.addAll(collection);
+
+        return set.equals(compareSet);
+        // // test for equal size
+        // // because collection can have duplicates, test for < rather than !=
+        // if (collection.size() < set.size()) {
+        // return false;
+        // }
+        //
+        // for (T member : collection) {
+        // if (!set.contains(member)) {
+        // return false;
+        // }
+        // }
+        //
+        // return true;
+
+    }
 }
