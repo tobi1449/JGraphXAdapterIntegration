7f1476438a3da2800866964348cb7d16c268b284
Lukas Eichkorn <leichkorn@gmx.de>
2013-06-01 17:08:26 +0200

    Forgot to add KonTabbedPane to the commit
    my bad

--- src/de/konsteirama/isgci/KonTabbedPane.java
+++ src/de/konsteirama/isgci/KonTabbedPane.java
@@ -1,10 +1,18 @@
 package de.konsteirama.isgci;
 
+import java.util.ArrayList;
+
 import javax.swing.JTabbedPane;
 
 import de.konsteirama.drawinglibrary.DrawingLibraryInterface;
 import de.konsteirama.drawinglibrary.JGraphXInterface;
 
+/**
+ * Represents the TabbedPane, beneath the Toolbar.
+ * 
+ * @author highyield
+ *
+ */
 public class KonTabbedPane extends JTabbedPane {
 
     /**
@@ -12,23 +20,51 @@ public class KonTabbedPane extends JTabbedPane {
      */
     private static final long serialVersionUID = -6438063674744178339L;
 
-    JGraphXInterface graphInterface1;
+    /**
+     * A ArrayList of all currently available JGraphXInterfaces
+     * connected via a Tab.
+     */
+    private ArrayList<JGraphXInterface> graphInterfaces;
     
-    JGraphXInterface graphInterface2;
-    
+    /**
+     * The constructor of KonTabbedPane.
+     * 
+     * Creates two tabs and adds a JGraphXInterface to each one
+     */
     public KonTabbedPane() {
-        // add a tab
-        JGraphXInterface graphInterface1 = new JGraphXInterface();
-        add(graphInterface1.getPanel());
-        addTab("First Tab", graphInterface1.getPanel());
-
-        // add a second tab
-        graphInterface2 = new JGraphXInterface();
-        add(graphInterface2.getPanel());
-        addTab("Second Tab", graphInterface2.getPanel());
+        // Initialize the ArrayList
+        graphInterfaces = new ArrayList<JGraphXInterface>();
+        
+        /*
+         * Adds a tab
+         */
+        JGraphXInterface graphInterface = new JGraphXInterface();
+        graphInterfaces.add(graphInterface);
+        
+        // Adds the graph to the tab and adds the tab to the pane
+        add(graphInterface.getPanel());
+        addTab("First Tab", graphInterface.getPanel());
+        
+        /*
+         * Adds a tab
+         */
+        graphInterface = new JGraphXInterface();
+        graphInterfaces.add(graphInterface);
+        
+        // Adds the graph to the tab and adds the tab to the pane
+        add(graphInterface.getPanel());
+        addTab("Second Tab", graphInterface.getPanel());  
     }
     
-    public DrawingLibraryInterface getActiveGraphDrawing() {
-        return graphInterface2;
+    /**
+     * Returns the current DrawingLibraryInterface 
+     * referenced by the current active tab.
+     * 
+     * @return an instance of DrawingLibraryInterface
+     */
+    public final DrawingLibraryInterface getActiveGraphDrawing() {
+        int selectedIndex = this.getSelectedIndex();
+                
+        return this.graphInterfaces.get(selectedIndex);
     }
 }
