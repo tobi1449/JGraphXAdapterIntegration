45b750b4d1071fb282ca7ae8d8f73c024508cdd8
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2013-06-01 17:38:16 +0200

    Extended JGraphXAdapterTest
    Fixed unachievable tests
    Separated genericTest

--- src/de/konsteirama/tests/JGraphXAdapterTests.java
+++ src/de/konsteirama/tests/JGraphXAdapterTests.java
@@ -2,6 +2,7 @@ package de.konsteirama.tests;
 
 import static org.junit.Assert.fail;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -10,6 +11,7 @@ import java.util.Set;
 import org.jgrapht.ListenableGraph;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.ListenableDirectedGraph;
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.mxgraph.model.mxCell;
@@ -43,6 +45,7 @@ public class JGraphXAdapterTests {
         jGraphT.addVertex(v3);
         jGraphT.addVertex(v4);
 
+        final int expectedEdges = 5;
         jGraphT.addEdge(v1, v2);
         jGraphT.addEdge(v1, v3);
         jGraphT.addEdge(v1, v4);
@@ -53,83 +56,95 @@ public class JGraphXAdapterTests {
         JGraphXAdapter<String, DefaultEdge> graphX = 
                 new JGraphXAdapter<String, DefaultEdge>(jGraphT);
         testMapping(graphX);
+        
+        // test if all values are in the jgraphx graph
+        Object[] expectedArray = {v1, v2, v3, v4};
+        Arrays.sort(expectedArray);
+        
+        Object[] realArray = graphX.getCellToVertexMap().values().toArray(); 
+        Arrays.sort(realArray);
+        Assert.assertArrayEquals(expectedArray, realArray);
+        
+        realArray = graphX.getVertexToCellMap().keySet().toArray();
+        Arrays.sort(realArray);
+        Assert.assertArrayEquals(expectedArray, realArray);
+        
+        int edgesCount = graphX.getCellToEdgeMap().values().size();
+        Assert.assertEquals(expectedEdges, edgesCount);
+        
+        edgesCount = graphX.getEdgeToCellMap().keySet().size();
+        Assert.assertEquals(expectedEdges, edgesCount);
+    }
 
-        try {
-            graphX.addJGraphTEdge(new DefaultEdge());
-            graphX.addJGraphTEdge(new DefaultEdge());
-            graphX.addJGraphTEdge(new DefaultEdge());
 
-            graphX.addJGraphTVertex("New Vertex 1");
-            graphX.addJGraphTVertex("New Vertex 2");
-            graphX.addJGraphTVertex("New Vertex 3");
-
-            // Add the same vertex 2 times
-            String v = "Some Vertex";
-            graphX.addJGraphTVertex(v);
-            graphX.addJGraphTVertex(v);
-
-            // Add the same edge 2 times
-            DefaultEdge edge = new DefaultEdge();
-            graphX.addJGraphTEdge(edge);
-            graphX.addJGraphTEdge(edge);
-
-        } catch (Exception e) {
-            fail("Unexpected error during adding new edges or vertices");
-        }
-
-        testMapping(graphX);
-
+    /**
+     * Tests the correct implementation of the GraphListener interface.
+     */
+    @Test
+    public final void listenerTest() {
+        ListenableGraph<String, String> jGraphT 
+            = new ListenableDirectedGraph<String, String>(String.class);
+        
+        JGraphXAdapter<String, String> graphX 
+            = new JGraphXAdapter<String, String>(jGraphT);
+        
         // add some data to the jgrapht graph - changes should be propagated
         // through jgraphxadapters graphlistener interface
 
-        String v5 = "Vertex 5";
-        String v6 = "Vertex 6";
-        String v7 = "Vertex 7";
-        String v8 = "Vertex 8";
+        String v1 = "Vertex 1";
+        String v2 = "Vertex 2";
+        String v3 = "Vertex 3";
+        String v4 = "Vertex 4";
 
-        jGraphT.addVertex(v5);
-        jGraphT.addVertex(v6);
-        jGraphT.addVertex(v7);
-        jGraphT.addVertex(v8);
+        jGraphT.addVertex(v1);
+        jGraphT.addVertex(v2);
+        jGraphT.addVertex(v3);
+        jGraphT.addVertex(v4);
 
-        jGraphT.addEdge(v5, v6);
-        jGraphT.addEdge(v5, v7);
-        jGraphT.addEdge(v5, v8);
-        jGraphT.addEdge(v6, v7);
-        jGraphT.addEdge(v7, v8);
+        final int expectedEdges = 5;
+        jGraphT.addEdge(v1, v2, "Edge 1");
+        jGraphT.addEdge(v1, v3, "Edge 2");
+        jGraphT.addEdge(v1, v4, "Edge 3");
+        jGraphT.addEdge(v2, v3, "Edge 4");
+        jGraphT.addEdge(v3, v4, "Edge 5");
 
         testMapping(graphX);
+        
+        // test if all values are in the jgraphx graph
+        Object[] expectedArray = {v1, v2, v3, v4};
+        Arrays.sort(expectedArray);
+        
+        Object[] realArray = graphX.getCellToVertexMap().values().toArray(); 
+        Arrays.sort(realArray);
+        Assert.assertArrayEquals(expectedArray, realArray);
+        
+        realArray = graphX.getVertexToCellMap().keySet().toArray();
+        Arrays.sort(realArray);
+        Assert.assertArrayEquals(expectedArray, realArray);
+        
+        int edgesCount = graphX.getCellToEdgeMap().values().size();
+        Assert.assertEquals(expectedEdges, edgesCount);
+        
+        edgesCount = graphX.getEdgeToCellMap().keySet().size();
+        Assert.assertEquals(expectedEdges, edgesCount);
     }
-
+    
+    
     /**
      * Tests conditions if graph is initialized without a JgraphT graph.
      */
     @Test
     public final void nullInitializationTest() {
-        JGraphXAdapter<String, String> graph = null;
-
         try {
-            graph = new JGraphXAdapter<String, String>(null);
+            new JGraphXAdapter<String, String>(null);
+            fail("Expected illegal argument exception");
+        } catch (IllegalArgumentException e) {
+            // expected result
+            Assert.assertTrue(true);
         } catch (Exception e) {
             fail("Unexpected error encountered during "
                     + " creation of JGraphXAdapter with null");
         }
-
-        testMapping(graph);
-
-        try {
-            graph.addJGraphTEdge("New Edge 1");
-            graph.addJGraphTEdge("New Edge 2");
-            graph.addJGraphTEdge("New Edge 3");
-
-            graph.addJGraphTVertex("New Vertex 1");
-            graph.addJGraphTVertex("New Vertex 2");
-            graph.addJGraphTVertex("New Vertex 3");
-        } catch (Exception e) {
-            fail("Unexpected error during adding new edges or vertices");
-        }
-
-        testMapping(graph);
     }
 
     /**
@@ -198,10 +213,8 @@ public class JGraphXAdapterTests {
     }
 
     /**
-     * Compares a collection to a set. Returns true if the specified parameters
-     * have the same size, and every member of the specified parameters is
-     * contained in this set (or equivalently, every member of the parameter is
-     * contained in the specified set).
+     * Compares a collection to a set by creating a new set from
+     * the collection and using equals. 
      * 
      * @param collection
      *            The collection that is compared
@@ -215,25 +228,12 @@ public class JGraphXAdapterTests {
      * @return True, if set and collection are equivalent; False if not.
      * 
      */
-    private <T> boolean compare(final Collection<T> collection, final Set<T> set) {
+    private <T> boolean compare(
+            final Collection<T> collection, final Set<T> set) {
 
         Set<T> compareSet = new HashSet<T>();
         compareSet.addAll(collection);
 
         return set.equals(compareSet);
-        // // test for equal size
-        // // because collection can have duplicates, test for < rather than !=
-        // if (collection.size() < set.size()) {
-        // return false;
-        // }
-        //
-        // for (T member : collection) {
-        // if (!set.contains(member)) {
-        // return false;
-        // }
-        // }
-        //
-        // return true;
-
     }
 }
