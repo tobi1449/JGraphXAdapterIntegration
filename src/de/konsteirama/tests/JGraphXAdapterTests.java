6c1e246ed63ae4f12741de68d7fb1678dffc619a
J. Weiser <jannis.weiser@gmail.com>
2013-06-03 17:15:05 +0200

    Added a test for not listenable Graphs.

--- src/de/konsteirama/tests/JGraphXAdapterTests.java
+++ src/de/konsteirama/tests/JGraphXAdapterTests.java
@@ -8,7 +8,9 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.jgrapht.Graph;
 import org.jgrapht.ListenableGraph;
+import org.jgrapht.graph.DefaultDirectedGraph;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.ListenableDirectedGraph;
 import org.junit.Assert;
@@ -179,6 +181,57 @@ public class JGraphXAdapterTests {
 
         testMapping(graphX);
 
+    }
+    
+    /**
+     * Tests if JGraphXAdapter works with not-listenable Graphs 
+     */
+    @Test
+    public final void notListenableTest(){
+    	Graph<String, String> jGraphT 
+    		= new DefaultDirectedGraph<String, String>(String.class);
+    	
+        // fill graph with data
+        String v1 = "Vertex 1";
+        String v2 = "Vertex 2";
+        String v3 = "Vertex 3";
+        String v4 = "Vertex 4";
+
+        jGraphT.addVertex(v1);
+        jGraphT.addVertex(v2);
+        jGraphT.addVertex(v3);
+
+        final int expectedEdges = 3;
+        jGraphT.addEdge(v1, v2, "Edge 1");
+        jGraphT.addEdge(v1, v3, "Edge 2");
+        jGraphT.addEdge(v2, v3, "Edge 3");
+        
+        JGraphXAdapter<String, String> graphX 
+            = new JGraphXAdapter<String, String>(jGraphT);        
+
+        jGraphT.addVertex(v4);
+        jGraphT.addEdge(v1, v4, "Edge 4");
+        jGraphT.addEdge(v3, v4, "Edge 5");        
+
+        testMapping(graphX);
+        
+        // test if all values are in the jgraphx graph
+        Object[] expectedArray = {v1, v2, v3};
+        Arrays.sort(expectedArray);
+        
+        Object[] realArray = graphX.getCellToVertexMap().values().toArray(); 
+        Arrays.sort(realArray);
+        Assert.assertArrayEquals(expectedArray, realArray);
+        
+        realArray = graphX.getVertexToCellMap().keySet().toArray();
+        Arrays.sort(realArray);
+        Assert.assertArrayEquals(expectedArray, realArray);
+        
+        int edgesCount = graphX.getCellToEdgeMap().values().size();
+        Assert.assertEquals(expectedEdges, edgesCount);
+        
+        edgesCount = graphX.getEdgeToCellMap().keySet().size();
+        Assert.assertEquals(expectedEdges, edgesCount);
     }
     
     // ========================Helper Methods===============================
