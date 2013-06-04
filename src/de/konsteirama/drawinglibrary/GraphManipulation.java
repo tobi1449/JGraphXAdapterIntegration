b56c9f7985211a28fc57eeaa370495d9f6452726
N. Siirak <Siina4242@googlemail.com>
2013-06-04 16:16:29 +0200

    Added implementation of some methods
    Implemented some listeners and methods for UNDO-Events and layout-stuff

--- src/de/konsteirama/drawinglibrary/GraphManipulation.java
+++ src/de/konsteirama/drawinglibrary/GraphManipulation.java
@@ -1,53 +1,84 @@
 package de.konsteirama.drawinglibrary;
 
+import com.mxgraph.layout.hierarchical.mxHierarchicalLayout;
 import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.util.mxEvent;
+import com.mxgraph.util.mxEventObject;
+import com.mxgraph.util.mxEventSource.mxIEventListener;
+import com.mxgraph.util.mxUndoManager;
+import com.mxgraph.util.mxUndoableEdit;
+import com.mxgraph.view.mxGraph;
 
 /**
- * This class implements the GraphManipulationInterface. It handles 
- * manipulations that are done on the JGraphX-graph that is viewed on the 
- * panel. Manipulation will be done by the user.
+ * This class implements the GraphManipulationInterface. It handles
+ * manipulations that are done on the JGraphX-graph that is viewed on the panel.
+ * Manipulation will be done by the user.
+ * 
  * @author Natascha
- *
+ * 
  */
 public class GraphManipulation implements GraphManipulationInterface {
 
 	/**
 	 * GraphComponent is the panel the graph is drawn in.
 	 */
-    private mxGraphComponent graphComponent;
-
-    /**
-     * Constructor of the class. Creates an instance of the
-     * GraphManipulation class that operates on a given
-     * graphComponent.
-     * @param graphComponent : a JGRaphX graphComponent, shown on the
-     * panel
-     */
-    public GraphManipulation(mxGraphComponent graphComponent) {
-        this.graphComponent = graphComponent;
-    }
+	private mxGraphComponent graphComponent;
 
 	/**
-	 * Returns a boolean denoting whether the calling graph is able
-	 * to perform a redo-operation.
+	 * manages the undo-operations on the calling graph
 	 */
-    @Override
-	public boolean canRedo() {
-		return false;
+	private mxUndoManager undoManager;
+
+	/**
+	 * Constructor of the class. Creates an instance of the GraphManipulation
+	 * class that operates on a given graphComponent.
+	 * 
+	 * @param graphComponent
+	 *            : a JGRaphX graphComponent, shown on the panel
+	 */
+	public GraphManipulation(mxGraphComponent graphComponent) {
+		this.graphComponent = graphComponent;
+
+		// initiation of undoManager variable
+		this.undoManager = new mxUndoManager();
+		
+		//notify undoManager about edits 
+		graphComponent.getGraph().getModel().addListener(mxEvent.UNDO, undoHandler);
+		graphComponent.getGraph().getView().addListener(mxEvent.UNDO, undoHandler);
+
 	}
 
-    /**
-     * Returns a boolean denoting whether the calling graph is able
-     * to perform an undo-operation.
-     */
+	protected mxIEventListener undoHandler = new mxIEventListener() {
+		public void invoke(Object source, mxEventObject evt) {
+			undoManager.undoableEditHappened((mxUndoableEdit) evt
+					.getProperty("edit"));
+		}
+	};
+
+	/**
+	 * Returns a boolean denoting whether the calling graph is able to perform a
+	 * redo-operation.
+	 * @return if falase then there was no undoable action perormed earlier
+	 */
+	@Override
+	public boolean canRedo() {
+		return undoManager.canRedo();
+	}
+
+	/**
+	 * Returns a boolean denoting whether the calling graph is able to perform
+	 * an undo-operation.
+	 */
 	@Override
 	public boolean canUndo() {
-		return false;
+		return undoManager.canUndo();
 	}
 
 	/**
 	 * Centers the view of the panel on the selected node.
-	 * @param node : a node of the graph
+	 * 
+	 * @param node
+	 *            : a node of the graph
 	 */
 	@Override
 	public void centerNode(Object node) {
@@ -56,31 +87,45 @@ public class GraphManipulation implements GraphManipulationInterface {
 
 	/**
 	 * Colors a given node in a given color.
-	 * @param node : a node of the graph
-	 * @param color : a color-parameter  
+	 * 
+	 * @param node
+	 *            : a node of the graph
+	 * @param color
+	 *            : a color-parameter
 	 */
 	@Override
 	public void colorNode(Object node, Object color) {
-		
+
 	}
 
 	/**
-	 * Marks the edge between two given nodes by adding a small grey 
-	 * arrow and coloring the edge.
-	 * @param node1 : node where the edge starts
-	 * @param node2 : node where the edge ends
+	 * Marks the edge between two given nodes by adding a small grey arrow and
+	 * coloring the edge.
+	 * 
+	 * @param node1
+	 *            : node where the edge starts
+	 * @param node2
+	 *            : node where the edge ends
 	 */
 	@Override
 	public void markEdge(Object node1, Object node2) {
-		
+
 	}
-	
+
 	/**
 	 * Gives a hierarchical order to the displayed graph.
 	 */
 	@Override
 	public void reapplyHierarchicalLayout() {
+		mxGraph graph = graphComponent.getGraph();
 		
+		graph.getModel().beginUpdate();
+		
+		mxHierarchicalLayout layout = new mxHierarchicalLayout(graph);
+		layout.execute(graph.getDefaultParent());
+		
+		graph.getModel().endUpdate();
+
 	}
 
 	/**
@@ -88,28 +133,34 @@ public class GraphManipulation implements GraphManipulationInterface {
 	 */
 	@Override
 	public void redo() {
-		
+		undoManager.redo();
+
 	}
 
 	/**
-	 * Removes the given node from the graph. Removal only effects
-	 * the JGraphX-graph.
-	 * @param node : a JGraphX-graph node object
+	 * Removes the given node from the graph. Removal only effects the
+	 * JGraphX-graph.
+	 * 
+	 * @param node
+	 *            : a JGraphX-graph node object
 	 */
 	@Override
 	public void removeNode(Object node) {
-		
+
 	}
 
 	/**
-	 * Alters the attribute name of a given node by replacing it
-	 * by a given new name. Renaming only effects the JGraphX-graph.
-	 * @param node : a node of the graph
-	 * @param newName : the name the node is given
+	 * Alters the attribute name of a given node by replacing it by a given new
+	 * name. Renaming only effects the JGraphX-graph.
+	 * 
+	 * @param node
+	 *            : a node of the graph
+	 * @param newName
+	 *            : the name the node is given
 	 */
 	@Override
 	public void renameNode(Object node, String newName) {
-		
+
 	}
 
 	/**
@@ -118,7 +169,7 @@ public class GraphManipulation implements GraphManipulationInterface {
 	 */
 	@Override
 	public void resetLayout() {
-		
+
 	}
 
 	/**
@@ -126,30 +177,36 @@ public class GraphManipulation implements GraphManipulationInterface {
 	 */
 	@Override
 	public void undo() {
-		
+		undoManager.undo();
+
 	}
 
 	/**
-	 * Zooms the panel to the given factor. It will magnify the graph, if the 
-	 * graph is too big for the panel only a 
-	 * section of the whole graph will be shown. This method zooms
-	 * to the center of the panel.
-	 * @param factor : a double that gives the zoom-factor
+	 * Zooms the panel to the given factor. It will magnify the graph, if the
+	 * graph is too big for the panel only a section of the whole graph will be
+	 * shown. This method zooms to the center of the panel.
+	 * 
+	 * @param factor
+	 *            : a double that gives the zoom-factor
 	 */
 	@Override
 	public void zoom(double factor) {
-		
+
 	}
 
 	/**
 	 * Zooms the panel to the given factor, centering on the given coordinates.
-	 * @param factor :  a double that gives the zoom-factor 
-	 * @param centerx : x-coordinate of the point zoom centers on
-	 * @param centery : y-coordinate of the point zoom centers on
+	 * 
+	 * @param factor
+	 *            : a double that gives the zoom-factor
+	 * @param centerx
+	 *            : x-coordinate of the point zoom centers on
+	 * @param centery
+	 *            : y-coordinate of the point zoom centers on
 	 */
 	@Override
 	public void zoom(double factor, double centerx, double centery) {
-		
+
 	}
 
 }
